0a87927bd9c2b16588eb5dc7af6f48a2691038b4
PeasKeeper <ymedvedev163@gmail.com>
2026-05-21 18:34:54 +0300

    Added FTXUI library

---
+++ third_party/FTXUI/cmake/ftxui_modules.cmake
@@ -0,0 +1,29 @@
+if (NOT FTXUI_BUILD_MODULES)
+  return()
+endif()
+
+add_library(ftxui-modules)
+
+target_sources(ftxui-modules
+  PUBLIC FILE_SET CXX_MODULES FILES
+  src/ftxui/ftxui.cppm
+  src/ftxui/component.cppm
+  src/ftxui/dom.cppm
+  src/ftxui/screen.cppm
+  src/ftxui/util.cppm
+  )
+
+target_link_libraries(ftxui-modules
+  PUBLIC
+  ftxui::screen
+  ftxui::dom
+  ftxui::component
+  )
+
+target_compile_features(ftxui-modules PUBLIC cxx_std_20)
+# TODO: Explain why this is needed.
+if (CMAKE_COMPILER_IS_GNUCXX)
+  target_compile_options(ftxui-modules PUBLIC -fmodules-ts)
+endif ()
+
+add_library(ftxui::modules ALIAS ftxui-modules)
